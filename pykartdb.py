ed79c69d17a334e0a6b09a1b26f628dba9896a04
mw24 <mw24@lonewolf.(none)>
2014-01-12 22:19:27 +0100

    Added commit line, extended listTrackDetails

--- pykartdb.py
+++ pykartdb.py
@@ -10,24 +10,39 @@ import sqlite3
 def connectDB():
     global con
     global cur    
-    con = sqlite3.connect('pykartdb.db')
+    con = sqlite3.connect('''pykartdb.db''')
     cur = con.cursor()
 
 
 def listAllTracks():
-    cur.execute('SELECT * FROM tracks;')
+    cur.execute('''SELECT * FROM tracks;''')
     tracklist = cur.fetchall()
     return tracklist
 
 def addTrack(trackName, trackLength, trackCity):
-    print("Adding track: ", trackName, trackLength, trackCity)
     cur.execute('''INSERT INTO tracks (name, length, city) values ("{0}", "{1}", "{2}");'''.format(trackName, trackLength, trackCity))
+    con.commit()
     
 
-def listTrackDetails(trackname):
-    cur.execute('SELECT * FROM tracks where name = "{0}";'.format(trackname))
-    trackdetails = cur.fetchone()
-    return trackdetails
+def listTrackDetails(trackName):
+    # get the track details
+    cur.execute('''SELECT * from tracks where name = "{0}";'''.format(trackName)
+    trackDetails = cur.fetchone()
+    trackID = trackDetails[0][0]
+    trackLength = trackDetails[0][2]
+    trackCity = trackDetails[0][3]
+    
+    # get total number of laps
+    cur.execute(''' SELECT ''')
+    
+    # get fastest lap
+
+    # Get total time spent on track
+
+    # Get total km or m total driver on track
+
+    #return all the values in a named tuple?
+    return trackLength
 
 # close the db connection
 def closeDB():
